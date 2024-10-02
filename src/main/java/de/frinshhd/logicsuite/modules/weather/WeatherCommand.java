4d3e44cc8ee08159d542bae9de1b885a7419a742
FrinshHD <frinshhd@gmail.com>
2024-10-02 18:34:16 +0200

    fixed chat / weather command

--- src/main/java/de/frinshhd/logicsuite/modules/weather/WeatherCommand.java
+++ src/main/java/de/frinshhd/logicsuite/modules/weather/WeatherCommand.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 public class WeatherCommand extends SpigotCommandExecutor {
     public WeatherCommand() {
-        super("chat");
+        super("weather");
     }
 
     @Override
